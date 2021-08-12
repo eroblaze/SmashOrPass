2102ef8c8c73f073d8a5a0861a0466186fbbfd4b
Paul Eboselume <eboselumepaul@gmail.com>
2021-08-11 20:24:42 -0700

    new changes

--- SOP/settings.py
+++ SOP/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+SECRET_KEY = 'django-insecure-h^+n$x3njs)ryf1)i9qmi@+t^!xh(@&453r9hy2kh-fvxlb)tr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
